c2ea70d5a776d5dbc1e3794b4cd4ae3af2eb66b3
Carey Ogola <ogolacarey254@gmail.com>
2025-03-12 14:37:16 +0300

    Nav-Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,32 +11,32 @@ const Footer = () => {
       <div className='bg-blue-950 flex flex-row justify-between text-gray-300 p-8'>
         <div className='grid grid-cols-3'>
           <div className='bg-inherit flex flex-col gap-2'>
-            <a><h3 className='text-2xl font-medium'>UEFA.COM</h3></a>
-            <a><h3>About</h3></a>
-            <a><h3>National associations</h3></a>
-            <a><h3>Running competitions</h3></a>
-            <a><h3>Development</h3></a>
-            <a><h3>Sustainability</h3></a>
-            <a><h3>News & Media</h3></a>
-            <a><h3></h3></a>
+            <h3 className='text-2xl font-medium'>UEFA.COM</h3>
+            <a className='hover:text-blue-400' href='#'>About</a>
+            <a className='hover:text-blue-400' href='#'>National associations</a>
+            <a className='hover:text-blue-400' href='#'>Running competitions</a>
+            <a className='hover:text-blue-400' href='#'>Development</a>
+            <a className='hover:text-blue-400' href='#'>Sustainability</a>
+            <a className='hover:text-blue-400' href='#'>News & Media</a>
+            
           </div>
         <div className='bg-inherit flex flex-col gap-2'>
-            <a><h3 className='text-2xl font-medium'>EXPLORE</h3></a>
-            <a><h3>UEFA.tv</h3></a>
-            <a><h3>Match Calendar</h3></a>
-            <a><h3>Rankings</h3></a>
-            <a><h3>Tickets/Hospitality</h3></a>
-            <a><h3>UEFA club competitions store</h3></a>
-            <a><h3>UEFA National football competitions store</h3></a>
+            <h3 className='text-2xl font-medium '>EXPLORE </h3>
+            <a className='hover:text-blue-400' href='#'>UEFA.tv</a>
+            <a className='hover:text-blue-400' href='#'>Match Calendar</a>
+            <a className='hover:text-blue-400' href='#'>Rankings</a>
+            <a className='hover:text-blue-400' href='#'>Tickets/Hospitality</a>
+            <a className='hover:text-blue-400' href='#'>UEFA club competitions store</a>
+            <a className='hover:text-blue-400' href='#'>UEFA National football competitions store</a>
         </div>
         <div className='bg-inherit flex flex-col gap-2'>
-            <a><h3 className='text-2xl font-medium'>MORE</h3></a>
-            <a><h3>MyUEFA</h3></a>
-            <a><h3>UC3</h3></a>
+            <h3 className='text-2xl font-medium'>MORE</h3>
+            <a className='hover:text-blue-400' href='#'>MyUEFA</a>
+            <a className='hover:text-blue-400' href='#'>UC3</a>
         </div>
       </div>
       <div className='bg-inherit flex flex-col'>
-        <a><h3 className='uppercase'>Change Language</h3></a>
+        <h3 className='uppercase'>Change Language</h3>
         <button className='py-4 w-52 rounded-full flex flex-row text-white items-center gap-3'>
           English
           <IoCaretDownCircleOutline />
@@ -44,19 +44,19 @@ const Footer = () => {
        <div className='flex flex-col'>
        <h2>FOLLOW US ON</h2>
        <div className='flex flex-row'>
-       <PiFacebookLogoFill size={30}/>
-       <BiLogoInstagramAlt size={30}/>
-       <RiTwitterXLine size={30}/>
-       <FaYoutube size={30}/>
+       <a className='hover:text-blue-400' href='#'><PiFacebookLogoFill size={30}/></a>
+       <a className='hover:text-blue-400' href='#'><BiLogoInstagramAlt size={30}/></a>
+       <a className='hover:text-blue-400' href='#'><RiTwitterXLine size={30}/></a>
+       <a className='hover:text-blue-400' href='#'><FaYoutube size={30}/></a>
        </div>
        </div>
       </div>
       </div>
       {/* the bottom-footer-section nested inside the main-footer */}
       <div className='bg-blue-900 flex flex-row items-center justify-center gap-5 py-12 text-gray-200'>
-        <a><h3>Terms and conditions</h3></a>
-        <a><h3>Privacy Policies</h3></a>
-        <a><h3>Cookie Policy</h3></a>
+        <a className='hover:text-blue-400' href='#'><h3>Terms and conditions</h3></a>
+        <a className='hover:text-blue-400' href='#'><h3>Privacy Policies</h3></a>
+        <a className='hover:text-blue-400' href='#'><h3>Cookie Policy</h3></a>
       </div>
     </footer>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import { IoPersonCircleOutline } from "react-icons/io5"
 const Navbar = () => {
   return (
     <nav className='bg-blue-800 w-full h-16 flex flex-row items-center justify-between px-4'>
-        <a><h3 className='text-white text-3xl font-bold'>UEFA.com</h3></a>
-        <a><h3 className='text-white hover:text-blue-400'>About</h3></a>
-        <a><h3 className='text-white hover:text-blue-400'>National associations</h3></a>
-        <a><h3 className='text-white hover:text-blue-400'>Running competitions</h3></a>
-        <a><h3 className='text-white hover:text-blue-400'>Development</h3></a>
-        <a><h3 className='text-white hover:text-blue-400'>Sustainability</h3></a>
-        <a><h3 className='text-white hover:text-blue-400'>News & Media</h3></a>
+        <a href='#' className='text-white text-3xl font-bold'>UEFA.com</a>
+        <a href='#' className='text-white hover:text-blue-400'>About</a>
+        <a href='#' className='text-white hover:text-blue-400'>National associations</a>
+        <a href='#' className='text-white hover:text-blue-400'>Running competitions</a>
+        <a href='#' className='text-white hover:text-blue-400'>Development</a>
+        <a href='#' className='text-white hover:text-blue-400'>Sustainability</a>
+        <a href='#' className='text-white hover:text-blue-400'>News & Media</a>
         <button className='w-24 items-center flex flex-row gap-2 bg-inherit text-white border border-white rounded-full py-2 px-2'>
           Log in
           <IoPersonCircleOutline size={20}/>
